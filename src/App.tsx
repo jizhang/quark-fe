7db6d09c4ec6e2d8f35f0ab8ae386442909ba5fa
Jerry <zhangji87@gmail.com>
2023-04-25 10:19:33 +0800

    App bar & account list

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,55 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+import Box from '@mui/material/Box'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import MenuIcon from '@mui/icons-material/Menu'
+import List from '@mui/material/List'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+import ListSubheader from '@mui/material/ListSubheader'
+import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction'
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Vite.js example in TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">Quark</Typography>
+        </Toolbar>
+      </AppBar>
+      <List>
+        <ListSubheader>Assets</ListSubheader>
+        <ListItemButton>
+          <ListItemText>Cash</ListItemText>
+          <ListItemSecondaryAction>350.00</ListItemSecondaryAction>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemText>CMB</ListItemText>
+          <ListItemSecondaryAction>224,453.89</ListItemSecondaryAction>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemText>Alipay</ListItemText>
+          <ListItemSecondaryAction>500,174.16</ListItemSecondaryAction>
+        </ListItemButton>
+        <ListSubheader>Liabilities</ListSubheader>
+        <ListItemButton>
+          <ListItemText>CMB credit card</ListItemText>
+          <ListItemSecondaryAction>11.00</ListItemSecondaryAction>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemText>Home loan</ListItemText>
+          <ListItemSecondaryAction>744,519.99</ListItemSecondaryAction>
+        </ListItemButton>
+      </List>
+    </Box>
   );
 }
