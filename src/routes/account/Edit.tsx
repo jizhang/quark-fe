ec8383f9e3c31888122360d74c303deaf79709b1
Jerry <zhangji87@gmail.com>
2023-04-29 12:32:39 +0800

    Validate form

--- src/routes/account/Edit.tsx
+++ src/routes/account/Edit.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import _ from 'lodash'
 import { useNavigate } from 'react-router-dom'
-import { Box, Stack, Button, TextField, FormControl, FormControlLabel, FormLabel, RadioGroup, Radio } from '@mui/material'
+import { Box, Stack, Button, TextField, FormControl, FormControlLabel, FormLabel, FormHelperText, RadioGroup, Radio } from '@mui/material'
 import * as service from '@/services/account'
 import Nav from '@/components/Nav'
 
@@ -11,6 +12,12 @@ export default () => {
     name: '',
     type: '1',
     balance: '0',
+  })
+
+  const [errors, setErrors] = useState({
+    name: '',
+    type: '',
+    balance: '',
   })
 
   function handleChangeName(event: React.ChangeEvent<HTMLInputElement>) {
@@ -36,6 +43,30 @@ export default () => {
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
+
+    const errors = {
+      name: '',
+      type: '',
+      balance: '',
+    }
+
+    if (!form.name) {
+      errors.name = 'Account name cannot be empty.'
+    }
+
+    if (!_.includes(['1', '2'], form.type)) {
+      errors.type = 'Invalid account type'
+    }
+
+    if (!form.balance) {
+      errors.balance = 'Initial balance cannot be empty.'
+    } else if (!_.isFinite(_.toNumber(form.balance))) {
+      errors.balance = 'Invalid number format'
+    }
+
+    setErrors(errors)
+    if (_(errors).values().some()) return
+
     service.saveAccount(form).then(() => {
       navigate('/')
     })
@@ -45,15 +76,29 @@ export default () => {
     <Box>
       <Nav title="Add Acount" />
       <Stack px={2} py={3} spacing={2} component="form" onSubmit={handleSubmit}>
-        <TextField label="Name" autoFocus value={form.name} onChange={handleChangeName}></TextField>
-        <FormControl>
+        <TextField
+          label="Name"
+          autoFocus
+          value={form.name}
+          onChange={handleChangeName}
+          error={!!errors.name}
+          helperText={errors.name}
+        ></TextField>
+        <FormControl error={!!errors.type}>
           <FormLabel>Type</FormLabel>
           <RadioGroup row value={form.type} onChange={handleChangeType}>
             <FormControlLabel value="1" control={<Radio />} label="Asset" />
             <FormControlLabel value="2" control={<Radio />} label="Liability" />
           </RadioGroup>
+          <FormHelperText>{errors.type}</FormHelperText>
         </FormControl>
-        <TextField label="Initial Balance" value={form.balance} onChange={handleChangeBalance}></TextField>
+        <TextField
+          label="Initial Balance"
+          value={form.balance}
+          onChange={handleChangeBalance}
+          error={!!errors.balance}
+          helperText={errors.balance}
+        ></TextField>
         <Stack spacing={2} direction="row">
           <Button variant="contained" type="submit">Save</Button>
           <Button onClick={() => { navigate('/') }}>Cancel</Button>
