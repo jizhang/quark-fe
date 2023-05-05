5220ea32de0c129cb1607225350eb121f2778f7f
Jerry <zhangji87@gmail.com>
2023-05-05 19:57:56 +0800

    Record list UI

--- src/routes/record/List.tsx
+++ src/routes/record/List.tsx
@@ -1,8 +1,19 @@
 import {
   Box,
+  Avatar,
   List,
-  ListItemButton
+  ListItem,
+  ListItemButton,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
 } from '@mui/material'
+import {
+  Upload as UploadIcon,
+  Download as DownloadIcon,
+  SwapHoriz,
+  ArrowRight,
+} from '@mui/icons-material'
 import Nav from '@/components/RecordListNav'
 
 export default () => {
@@ -10,9 +21,24 @@ export default () => {
     <Box>
       <Nav />
       <List>
-        <ListItemButton>Item 1</ListItemButton>
-        <ListItemButton>Item 2</ListItemButton>
-        <ListItemButton>Item 3</ListItemButton>
+        <ListItem disablePadding secondaryAction="10.90">
+          <ListItemButton>
+            <ListItemAvatar><Avatar><UploadIcon /></Avatar></ListItemAvatar>
+            <ListItemText primary="Mobile" secondary="May 5 08:17 - Remark" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding secondaryAction="12,345.67">
+          <ListItemButton>
+            <ListItemAvatar><Avatar><DownloadIcon /></Avatar></ListItemAvatar>
+            <ListItemText primary="Salary" secondary="May 4 17:13 - Remark" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding secondaryAction="1,000.00">
+          <ListItemButton>
+            <ListItemAvatar><Avatar><SwapHoriz /></Avatar></ListItemAvatar>
+            <ListItemText primary={<Typography display="flex">CMB<ArrowRight />Alipay</Typography>} secondary="May 3 06:32"/>
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   )
