ba778276284694807c364f699852d374de72f3d3
Jerry <zhangji87@gmail.com>
2023-05-12 15:54:43 +0800

    Remove googleapis, fix #8

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import theme from './theme';
