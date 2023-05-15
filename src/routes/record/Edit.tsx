4be165f9792dc19e57a15bb837acc00189a9ebd6
Jerry <zhangji87@gmail.com>
2023-05-15 20:23:29 +0800

    Use formik & yup for validation (#15)
    * Use formik & yup
    
    * Update record form

--- src/routes/record/Edit.tsx
+++ src/routes/record/Edit.tsx
@@ -17,7 +17,6 @@ import {
   Button,
   InputLabel,
   Select,
-  SelectChangeEvent,
   MenuItem,
 } from '@mui/material'
 import { ArrowBackIos } from '@mui/icons-material'
@@ -25,23 +24,13 @@ import { DateTimePicker } from '@mui/x-date-pickers'
 import { useConfirm } from "material-ui-confirm";
 import _ from 'lodash'
 import dayjs, { Dayjs } from 'dayjs'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 import * as consts from '@/common/consts'
 import * as userService from '@/services/user'
 import * as accountService from '@/services/account'
 import * as categoryService from '@/services/category'
 import * as service from '@/services/record'
-
-function getEmptyErrors() {
-  return {
-    recordType: '',
-    categoryId: '',
-    accountId: '',
-    targetAccountId: '',
-    recordTime: '',
-    amount: '',
-    remark: '',
-  }
-}
 
 export default () => {
   const navigate = useNavigate()
@@ -52,13 +41,13 @@ export default () => {
   useEffect(() => {
     if (recordId) {
       service.getRecord(_.toInteger(recordId)).then(payload => {
-        setForm({
+        form.setValues({
           id: String(payload.id),
-          recordType: String(payload.record_type),
-          categoryId: payload.category_id ? String(payload.category_id) : '',
-          accountId: String(payload.account_id),
-          targetAccountId: payload.target_account_id ? String(payload.target_account_id) : '',
-          recordTime: dayjs(payload.record_time),
+          record_type: String(payload.record_type),
+          category_id: payload.category_id ? String(payload.category_id) : '',
+          account_id: String(payload.account_id),
+          target_account_id: payload.target_account_id ? String(payload.target_account_id) : '',
+          record_time: dayjs(payload.record_time),
           amount: String(payload.amount),
           remark: String(payload.remark),
         })
@@ -66,130 +55,76 @@ export default () => {
     }
   }, [recordId])
 
-  const [form, setForm] = useState({
-    id: '',
-    recordType: '1',
-    categoryId: '',
-    accountId: '',
-    targetAccountId: '',
-    recordTime: dayjs() as Dayjs | null,
-    amount: '',
-    remark: '',
+  const form = useFormik({
+    initialValues: {
+      id: '',
+      record_type: '1',
+      category_id: '',
+      account_id: '',
+      target_account_id: '',
+      record_time: dayjs() as Dayjs | null,
+      amount: '',
+      remark: '',
+    },
+
+    validationSchema: yup.object({
+      record_type: yup.number().required('Record type cannot be empty.'),
+      category_id: yup.number()
+        .when('record_type', {
+          is: (value: number) => value === consts.RECORD_TYPE_EXPENSE || value === consts.RECORD_TYPE_INCOME,
+          then: schema => schema.required('Category cannot be empty.'),
+        }),
+      account_id: yup.number()
+        .when('record_type', {
+          is: (value: number) => value === consts.RECORD_TYPE_EXPENSE || value === consts.RECORD_TYPE_INCOME,
+          then: schema => schema.required('Account cannot be empty.'),
+        })
+        .when('record_type', {
+          is: consts.RECORD_TYPE_TRANSFER,
+          then: schema => schema.required('Source account cannot be empty.'),
+        }),
+      target_account_id: yup.number()
+        .when('record_type', {
+          is: consts.RECORD_TYPE_TRANSFER,
+          then: schema => schema.required('Target account cannot be empty.'),
+        }),
+      record_time: yup.mixed().required('Record time cannot be empty.'),
+      amount: yup.number()
+        .required('Amount cannot be empty.')
+        .typeError('Invalid number format'),
+      remark: yup.string(),
+    }),
+
+    onSubmit: (values) => {
+      const recordForm = {
+        id: values.id ? _.toInteger(values.id) : undefined,
+        record_type: _.toInteger(values.record_type),
+        category_id: values.category_id ? _.toInteger(values.category_id) : undefined,
+        account_id: _.toInteger(values.account_id),
+        target_account_id: values.target_account_id ? _.toInteger(values.target_account_id) : undefined,
+        record_time: (values.record_time || dayjs()).format('YYYY-MM-DD HH:mm:ss'),
+        amount: _.toNumber(values.amount),
+        remark: values.remark,
+      }
+
+      service.saveRecord(recordForm).then(() => {
+        navigate('/record/list')
+      })
+    }
   })
 
-  const [errors, setErrors] = useState(getEmptyErrors())
-
   function handleChangeRecordType(event: React.ChangeEvent<HTMLInputElement>) {
-    setForm({
-      ...form,
-      recordType: event.target.value,
-      categoryId: '',
-    })
-  }
-
-  function handleChangeCategoryId(event: SelectChangeEvent) {
-    setForm({
-      ...form,
-      categoryId: event.target.value,
-    })
-  }
-
-  function handleChangeAccountId(event: SelectChangeEvent) {
-    setForm({
-      ...form,
-      accountId: event.target.value
-    })
-  }
-
-  function handleChangeTargetAccountId(event: SelectChangeEvent) {
-    setForm({
-      ...form,
-      targetAccountId: event.target.value
-    })
+    form.setFieldValue('record_type', event.target.value)
+    form.setFieldValue('category_id', '', false)
   }
 
   function handleChangeRecordTime(value: Dayjs | null) {
-    setForm({
-      ...form,
-      recordTime: value,
-    })
-  }
-
-  function handleChangeAmount(event: React.ChangeEvent<HTMLInputElement>) {
-    setForm({
-      ...form,
-      amount: event.target.value,
-    })
-  }
-
-  function handleChangeRemark(event: React.ChangeEvent<HTMLInputElement>) {
-    setForm({
-      ...form,
-      remark: event.target.value,
-    })
-  }
-
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-    const errors = getEmptyErrors()
-
-    if (!form.recordType) {
-      errors.recordType = 'Record type cannot be empty.'
-    }
-
-    if (form.recordType === String(consts.RECORD_TYPE_EXPENSE) || form.recordType === String(consts.RECORD_TYPE_INCOME)) {
-      if (!form.categoryId) {
-        errors.categoryId = 'Category cannot be empty.'
-      }
-      if (!form.accountId) {
-        errors.accountId = 'Account cannot be empty.'
-      }
-    } else if (form.recordType === String(consts.RECORD_TYPE_TRANSFER)) {
-      if (!form.accountId) {
-        errors.accountId = 'Source account cannot be empty.'
-      }
-      if (!form.targetAccountId) {
-        errors.targetAccountId = 'Target account cannot be empty.'
-      }
-    } else {
-      errors.recordType = 'Invalid record type'
-    }
-
-    if (!form.recordTime) {
-      errors.recordTime = 'Record time cannot be empty.'
-    }
-
-    if (!form.amount) {
-      errors.amount = 'Amount cannot be empty.'
-    } else if (!_.isFinite(_.toNumber(form.amount))) {
-      errors.amount = 'Invalid number format'
-    }
-
-    setErrors(errors)
-    if (_(errors).values().some()) {
-      return
-    }
-
-    const recordForm = {
-      id: form.id ? _.toInteger(form.id) : undefined,
-      record_type: _.toInteger(form.recordType),
-      category_id: form.categoryId ? _.toInteger(form.categoryId) : undefined,
-      account_id: _.toInteger(form.accountId),
-      target_account_id: form.targetAccountId ? _.toInteger(form.targetAccountId) : undefined,
-      record_time: (form.recordTime || dayjs()).format('YYYY-MM-DD HH:mm:ss'),
-      amount: _.toNumber(form.amount),
-      remark: form.remark,
-    }
-
-    service.saveRecord(recordForm).then(() => {
-      navigate('/record/list')
-    })
+    form.setFieldValue('record_time', value)
   }
 
   function handleDelete() {
     confirm().then(() => {
-      service.deleteRecord(_.toInteger(form.id)).then(() => {
+      service.deleteRecord(_.toInteger(form.values.id)).then(() => {
         navigate('/record/list')
       })
     }, _.noop)
@@ -215,10 +150,7 @@ export default () => {
     if (!recordId) {
       userService.getUserSetting().then(payload => {
         if (payload.default_account_id) {
-          setForm({
-            ...form,
-            accountId: String(payload.default_account_id),
-          })
+          form.setFieldValue('account_id', String(payload.default_account_id))
         }
       })
     }
@@ -241,86 +173,92 @@ export default () => {
           <Typography variant="h6" sx={{ flexGrow: 1 }}>Add Record</Typography>
         </Toolbar>
       </AppBar>
-      <Stack px={2} py={3} spacing={2} component="form" onSubmit={handleSubmit}>
-        <FormControl error={!!errors.recordType}>
+      <Stack px={2} py={3} spacing={2} component="form" onSubmit={form.handleSubmit}>
+        <FormControl error={form.touched.record_type && !!form.errors.record_type}>
           <FormLabel>Type</FormLabel>
-          <RadioGroup row value={form.recordType} onChange={handleChangeRecordType}>
+          <RadioGroup row name="record_type" value={form.values.record_type} onChange={handleChangeRecordType}>
             <FormControlLabel value={consts.RECORD_TYPE_EXPENSE} control={<Radio />} label="Expense" />
             <FormControlLabel value={consts.RECORD_TYPE_INCOME} control={<Radio />} label="Income" />
             <FormControlLabel value={consts.RECORD_TYPE_TRANSFER} control={<Radio />} label="Transfer" />
           </RadioGroup>
-          <FormHelperText>{errors.recordType}</FormHelperText>
+          <FormHelperText>{form.touched.record_type && form.errors.record_type}</FormHelperText>
         </FormControl>
-        {(form.recordType === String(consts.RECORD_TYPE_EXPENSE) || form.recordType === String(consts.RECORD_TYPE_INCOME)) && (
-          <FormControl error={!!errors.categoryId}>
+        {(form.values.record_type === String(consts.RECORD_TYPE_EXPENSE) || form.values.record_type === String(consts.RECORD_TYPE_INCOME)) && (
+          <FormControl error={form.touched.category_id && !!form.errors.category_id}>
             <InputLabel>Category</InputLabel>
             <Select
-              value={form.categoryId}
+              name="category_id"
+              value={form.values.category_id}
               label="Category"
-              onChange={handleChangeCategoryId}
+              onChange={form.handleChange}
             >
-              {_.filter(categories, ['type', _.toInteger(form.recordType)]).map(item => (
+              {_.filter(categories, ['type', _.toInteger(form.values.record_type)]).map(item => (
                 <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
               ))}
             </Select>
-            <FormHelperText>{errors.categoryId}</FormHelperText>
+            <FormHelperText>{form.touched.category_id && form.errors.category_id}</FormHelperText>
           </FormControl>
         )}
-        <FormControl error={!!errors.accountId}>
-          <InputLabel>{form.recordType === String(consts.RECORD_TYPE_TRANSFER) ? 'Source Account' : 'Account'}</InputLabel>
+        <FormControl error={form.touched.account_id && !!form.errors.account_id}>
+          <InputLabel>{form.values.record_type === String(consts.RECORD_TYPE_TRANSFER) ? 'Source Account' : 'Account'}</InputLabel>
           <Select
-            value={form.accountId}
-            label={form.recordType === String(consts.RECORD_TYPE_TRANSFER) ? 'Source Account' : 'Account'}
-            onChange={handleChangeAccountId}
+            name="account_id"
+            value={form.values.account_id}
+            label={form.values.record_type === String(consts.RECORD_TYPE_TRANSFER) ? 'Source Account' : 'Account'}
+            onChange={form.handleChange}
           >
             {accounts.map(item => (
               <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
             ))}
           </Select>
-          <FormHelperText>{errors.accountId}</FormHelperText>
+          <FormHelperText>{form.touched.account_id && form.errors.account_id}</FormHelperText>
         </FormControl>
-        {form.recordType === String(consts.RECORD_TYPE_TRANSFER) && (
-          <FormControl error={!!errors.targetAccountId}>
+        {form.values.record_type === String(consts.RECORD_TYPE_TRANSFER) && (
+          <FormControl error={form.touched.target_account_id && !!form.errors.target_account_id}>
             <InputLabel>Target Account</InputLabel>
             <Select
-              value={form.targetAccountId}
+              name="target_account_id"
+              value={form.values.target_account_id}
               label="Target Account"
-              onChange={handleChangeTargetAccountId}
+              onChange={form.handleChange}
             >
               {accounts.map(item => (
                 <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
               ))}
             </Select>
-            <FormHelperText>{errors.targetAccountId}</FormHelperText>
+            <FormHelperText>{form.touched.target_account_id && form.errors.target_account_id}</FormHelperText>
           </FormControl>
         )}
         <DateTimePicker
           label="Time"
-          value={form.recordTime}
+          value={form.values.record_time}
           onChange={handleChangeRecordTime}
           slotProps={{
             textField: {
-              error: !!errors.recordTime,
-              helperText: errors.recordTime,
+              name: 'record_time',
+              error: form.touched.record_time && !!form.errors.record_time,
+              helperText: form.touched.record_time && form.errors.record_time,
             },
           }}
         />
         <TextField
           label="Amount"
-          value={form.amount}
-          onChange={handleChangeAmount}
-          error={!!errors.amount}
-          helperText={errors.amount}
+          name="amount"
+          value={form.values.amount}
+          onChange={form.handleChange}
+          error={form.touched.amount && !!form.errors.amount}
+          helperText={form.touched.amount && form.errors.amount}
         ></TextField>
         <TextField
           label="Remark"
-          value={form.remark}
-          onChange={handleChangeRemark}
-          error={!!errors.remark}
-          helperText={errors.remark}
+          name="remark"
+          value={form.values.remark}
+          onChange={form.handleChange}
+          error={form.touched.remark && !!form.errors.remark}
+          helperText={form.touched.remark && form.errors.remark}
         ></TextField>
         <Button variant="contained" type="submit">Save</Button>
-        {form.id && (
+        {form.values.id && (
           <Button variant="outlined" color="error" onClick={handleDelete}>Delete</Button>
         )}
       </Stack>
