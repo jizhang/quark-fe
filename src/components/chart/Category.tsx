80b9313291a1b0442689348fda783e287eea6caa
Jerry <zhangji87@gmail.com>
2023-05-29 15:27:55 +0800

    Link category chart

--- src/components/chart/Category.tsx
+++ src/components/chart/Category.tsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import {
   List,
   ListSubheader,
   ListItem,
+  ListItemButton,
   ListItemText,
 } from '@mui/material'
+import qs from 'qs'
 import * as chartService from '@/services/chart'
 import TitleAmount from '@/components/TitleAmount'
 import Progress from './Progress'
@@ -22,6 +25,18 @@ export default (props: Props) => {
     })
   }, [props.month])
 
+  function makeLinkTo(group: chartService.ChartGroup, item: chartService.ChartItem) {
+    const search = {
+      record_type: group.id,
+      category_id: item.id,
+    }
+
+    return {
+      pathname: '/record/list',
+      search: qs.stringify(search),
+    }
+  }
+
   return (
     <List>
       {groups.map(group => (
@@ -30,10 +45,12 @@ export default (props: Props) => {
             <TitleAmount title={group.name} amount={group.amount} />
           </ListSubheader>
           {group.categories.map(item => (
-            <ListItem key={item.id}>
-              <ListItemText secondary={<Progress percent={item.percent} />}>
-                <TitleAmount title={item.name} amount={item.amount} />
-              </ListItemText>
+            <ListItem key={item.id} disablePadding>
+              <ListItemButton component={Link} to={makeLinkTo(group, item)}>
+                <ListItemText secondary={<Progress percent={item.percent} />}>
+                  <TitleAmount title={item.name} amount={item.amount} />
+                </ListItemText>
+              </ListItemButton>
             </ListItem>
           ))}
         </React.Fragment>
