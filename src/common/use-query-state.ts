b970385c287d7a453058570457f26c1b7b033fa5
Jerry <zhangji87@gmail.com>
2023-06-08 11:38:55 +0800

    Simplify useQueryState

--- src/common/use-query-state.ts
+++ src/common/use-query-state.ts
@@ -1,25 +1,15 @@
-import { useState, useEffect } from 'react'
+import { useMemo, useRef } from 'react'
 import { useSearchParams } from 'react-router-dom'
-import _ from 'lodash'
 
 export type QueryState = Record<string, string>
 export type SetQueryState = (newState: QueryState) => void
 
 export default (initialState: QueryState): [QueryState, SetQueryState] => {
+  const initialStateRef = useRef(initialState)
   const [searchParams, setSearchParams] = useSearchParams()
-
-  const [state, setState] = useState({
-    ...initialState,
+  const mergedState = useMemo(() => ({
+    ...initialStateRef.current,
     ...Object.fromEntries(searchParams),
-  })
-
-  useEffect(() => {
-    const mergedState = {
-      ...initialState,
-      ...Object.fromEntries(searchParams),
-    }
-    setState(prevState => _.isEqual(prevState, mergedState) ? prevState : mergedState)
-  }, [initialState, searchParams])
-
-  return [state, setSearchParams]
+  }), [initialStateRef, searchParams])
+  return [mergedState, setSearchParams]
 }
