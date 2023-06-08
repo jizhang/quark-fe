3f9192ff85489738aee04e09f3f3c5b2dfff510d
Jerry <zhangji87@gmail.com>
2023-06-08 09:13:47 +0800

    Add eslint react plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,11 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
@@ -9,5 +14,11 @@ module.exports = {
     semi: ['error', 'never'],
     'comma-dangle': ['error', 'always-multiline'],
     '@typescript-eslint/no-explicit-any': 'off',
+    'react/display-name': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
