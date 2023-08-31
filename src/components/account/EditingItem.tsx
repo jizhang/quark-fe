c628f3e2ecacf389ef0f40d8fe242acf316d3210
Jerry <zhangji87@gmail.com>
2023-08-31 09:27:58 +0800

    Add dnd-kit

--- src/components/account/EditingItem.tsx
+++ src/components/account/EditingItem.tsx
@@ -11,6 +11,25 @@ import {
   DragHandle,
 } from '@mui/icons-material'
 import { useConfirm } from 'material-ui-confirm'
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+  MouseSensor,
+  DragOverlay,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+  useSortable,
+} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
 import _ from 'lodash'
 import { formatAmount } from '@/common/utils'
 import * as accountService from '@/services/account'
@@ -25,6 +44,20 @@ export default (props: Props) => {
 
   const confirm = useConfirm()
   const navigate = useNavigate()
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    setActivatorNodeRef,
+  } = useSortable({id: account.id});
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
 
   function handleEdit() {
     navigate({
@@ -41,15 +74,18 @@ export default (props: Props) => {
     }, _.noop)
   }
 
-  function handleDragDrop() {
-    alert('Not implemented yet.')
-  }
-
   const iconSx = { p: 0, ml: 2 }
 
   return (
-    <ListItem>
-      <ListItemIcon onClick={handleDragDrop}>
+    <ListItem
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+    >
+      <ListItemIcon
+        ref={setActivatorNodeRef}
+        {...listeners}
+      >
         <DragHandle />
       </ListItemIcon>
       <ListItemText secondary={formatAmount(account.balance)}>{account.name}</ListItemText>
--- src/components/account/EditingList.tsx
+++ src/components/account/EditingList.tsx
@@ -5,6 +5,25 @@ import {
   ListItemText,
   ListSubheader,
 } from '@mui/material'
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+  MouseSensor,
+  DragOverlay,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+  useSortable,
+} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
 import _ from 'lodash'
 import { type AccountGroup } from '@/components/account/use-accounts'
 import EditingItem from './EditingItem'
@@ -14,12 +33,22 @@ interface Props {
 }
 
 export default (props: Props) => {
+  const sensors = useSensors(useSensor(TouchSensor), useSensor(MouseSensor))
+
   return (
-    <List>
-      <ListSubheader>{props.group.name}</ListSubheader>
-      {props.group.accounts.map(account => (
-        <EditingItem key={account.id} account={account} onDelete={_.noop} />
-      ))}
-    </List>
+    <DndContext
+      sensors={sensors}
+    >
+      <List>
+        <ListSubheader>{props.group.name}</ListSubheader>
+        <SortableContext
+          items={props.group.accounts}
+        >
+          {props.group.accounts.map(account => (
+            <EditingItem key={account.id} account={account} onDelete={_.noop} />
+          ))}
+        </SortableContext>
+      </List>
+    </DndContext>
   )
 }
